529cf557453034463729796569fa976873e1ea4d
sakshamtiwari2006 <saksham4141tiwari@gmail.com>
2026-08-13 12:50:48 +0530

    Update slave_ahb.v

--- slave_ahb.v
+++ slave_ahb.v
@@ -45,6 +45,7 @@ module slave_ahb(
     localparam WRITE_S2_STATE = 4'b1000; 
     localparam WRITE_S3_STATE = 4'b1001; 
     localparam WRITE_S4_STATE = 4'b1010; 
+    localparam ERROR_STATE    = 4'b1011; 
 
     reg [3:0] state;
 
@@ -62,32 +63,32 @@ module slave_ahb(
     reg [7:0] mem_wdata;
     wire [7:0] mem_rdata;
 
-    wire [9:0] base_addr = {addr_reg[9:2],2'b00};
+    wire [9:0] base_addr = {addr_reg[9:2], 2'b00};
 
     always @(posedge HCLK or negedge HRESETn) begin
         if (!HRESETn) begin
-            state <= IDLE_STATE;
-            addr_reg <= 0;
-            size_reg <= 0;
-            write_reg <= 0;
-            valid_packet <= 0;
-            mem_addr <= 0;
-            mem_we <= 0;
-            mem_wdata <= 0;
+            state         <= IDLE_STATE;
+            addr_reg      <= 0;
+            size_reg      <= 0;
+            write_reg     <= 0;
+            valid_packet  <= 0;
+            mem_addr      <= 0;
+            mem_we        <= 0;
+            mem_wdata     <= 0;
             internal_read <= 0;
-            byte_offset <= 0;
+            byte_offset   <= 0;
         end else begin
             mem_we <= 0;
 
             case (state)
                 IDLE_STATE: begin
                     if (HSEL && HTRANS[1] && HREADY) begin
-                        state <= SETUP_STATE;
-                        addr_reg <= HADDR;
-                        size_reg <= HSIZE;
-                        write_reg <= HWRITE;
+                        state        <= SETUP_STATE;
+                        addr_reg     <= HADDR;
+                        size_reg     <= HSIZE;
+                        write_reg    <= HWRITE;
                         valid_packet <= 1;
-                        byte_offset <= HADDR[1:0];
+                        byte_offset  <= HADDR[1:0];
                     end
                 end
 
@@ -95,14 +96,14 @@ module slave_ahb(
                     if (!error_hit) begin
                         mem_addr <= base_addr + byte_offset;
                         if (write_reg) begin
-                            mem_we <= 1;
+                            mem_we    <= 1;
                             mem_wdata <= HWDATA[7:0];
-                            state <= WRITE_S1_STATE;
+                            state     <= WRITE_S1_STATE;
                         end else begin
-                            state <= READ_S1_STATE;
+                            state     <= READ_S1_STATE;
                         end
                     end else begin
-                        state <= TRANSFER_STATE;
+                        state <= ERROR_STATE; 
                     end
                 end
 
@@ -112,7 +113,7 @@ module slave_ahb(
                         state <= TRANSFER_STATE;
                     else begin
                         mem_addr <= base_addr + byte_offset + 1;
-                        state <= READ_S2_STATE;
+                        state    <= READ_S2_STATE;
                     end
                 end
 
@@ -122,68 +123,71 @@ module slave_ahb(
                         state <= TRANSFER_STATE;
                     else begin
                         mem_addr <= base_addr + byte_offset + 2;
-                        state <= READ_S3_STATE;
+                        state    <= READ_S3_STATE;
                     end
                 end
 
                 READ_S3_STATE: begin
                     internal_read[23:16] <= mem_rdata;
-                    mem_addr <= base_addr + byte_offset + 3;
-                    state <= READ_S4_STATE;
+                    mem_addr             <= base_addr + byte_offset + 3;
+                    state                <= READ_S4_STATE;
                 end
 
                 READ_S4_STATE: begin
                     internal_read[31:24] <= mem_rdata;
-                    state <= TRANSFER_STATE;
+                    state                <= TRANSFER_STATE;
                 end
 
                 WRITE_S1_STATE: begin
-                    mem_we <= 1;
-                    mem_wdata <= HWDATA[7:0];
                     if (size_reg == 3'b000)
                         state <= TRANSFER_STATE;
                     else begin
-                        mem_addr <= base_addr + byte_offset + 1;
-                        state <= WRITE_S2_STATE;
+                        mem_we    <= 1;
+                        mem_wdata <= HWDATA[15:8];
+                        mem_addr  <= base_addr + byte_offset + 1;
+                        state     <= WRITE_S2_STATE;
                     end
                 end
 
                 WRITE_S2_STATE: begin
-                    mem_we <= 1;
-                    mem_wdata <= HWDATA[15:8];
                     if (size_reg == 3'b001)
                         state <= TRANSFER_STATE;
                     else begin
-                        mem_addr <= base_addr + byte_offset + 2;
-                        state <= WRITE_S3_STATE;
+                        mem_we    <= 1;
+                        mem_wdata <= HWDATA[23:16];
+                        mem_addr  <= base_addr + byte_offset + 2;
+                        state     <= WRITE_S3_STATE;
                     end
                 end
 
                 WRITE_S3_STATE: begin
-                    mem_we <= 1;
-                    mem_wdata <= HWDATA[23:16];
-                    mem_addr <= base_addr + byte_offset + 3;
-                    state <= WRITE_S4_STATE;
+                    mem_we    <= 1;
+                    mem_wdata <= HWDATA[31:24];
+                    mem_addr  <= base_addr + byte_offset + 3;
+                    state     <= WRITE_S4_STATE;
                 end
 
                 WRITE_S4_STATE: begin
-                    mem_we <= 1;
-                    mem_wdata <= HWDATA[31:24];
                     state <= TRANSFER_STATE;
                 end
 
                 TRANSFER_STATE: begin
                     if (HSEL && HTRANS[1] && HREADY) begin
-                        state <= SETUP_STATE;
-                        addr_reg <= HADDR;
-                        size_reg <= HSIZE;
-                        write_reg <= HWRITE;
+                        state        <= SETUP_STATE;
+                        addr_reg     <= HADDR;
+                        size_reg     <= HSIZE;
+                        write_reg    <= HWRITE;
                         valid_packet <= 1;
-                        byte_offset <= HADDR[1:0];
+                        byte_offset  <= HADDR[1:0];
                     end else begin
-                        state <= IDLE_STATE;
+                        state        <= IDLE_STATE;
                         valid_packet <= 0;
                     end
+                end
+
+                ERROR_STATE: begin
+                    state        <= IDLE_STATE; // End error cycle and return to IDLE
+                    valid_packet <= 0;
                 end
 
                 default: state <= IDLE_STATE;
@@ -213,27 +217,33 @@ module slave_ahb(
     );
 
     always @(*) begin
-        HRESP = 0;
+        HRESP  = 0;
         HRDATA = 0;
 
         case (state)
-            SETUP_STATE,
+            SETUP_STATE: begin
+                if (error_hit) begin
+                    HREADYOUT = 0;
+                    HRESP     = 1; 
+                end else begin
+                    HREADYOUT = 0;
+                end
+            end
+
+            ERROR_STATE: begin
+                HREADYOUT = 1; 
+                HRESP     = 1; 
+            end
+
             READ_S1_STATE, READ_S2_STATE, READ_S3_STATE, READ_S4_STATE,
             WRITE_S1_STATE, WRITE_S2_STATE, WRITE_S3_STATE, WRITE_S4_STATE: begin
                 HREADYOUT = 0;
-                if (state == SETUP_STATE && error_hit) begin
-                    HREADYOUT = 1;
-                    HRESP = 1;
-                end
             end
 
             TRANSFER_STATE: begin
                 HREADYOUT = 1;
 
-                if (valid_packet && error_hit)
-                    HRESP = 1;
-
-                else if (!write_reg) begin
+                if (!write_reg) begin
                     if (size_reg == 3'b010)
                         HRDATA = internal_read;
                     else if (size_reg == 3'b001)
@@ -244,8 +254,8 @@ module slave_ahb(
             end
 
             default: begin
-                HREADYOUT = 0;
-                HRESP = 0;
+                HREADYOUT = 1;
+                HRESP     = 0;
             end
         endcase
     end
